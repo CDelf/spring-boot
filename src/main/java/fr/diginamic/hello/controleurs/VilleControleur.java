624b292221aca6ac087a4a8451dfb7a636ab8945
Carole <c.delfino@live.fr>
2025-05-27 17:04:37 +0200

    tp 4 et 5

--- src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/VilleControleur.java
@@ -1,25 +1,80 @@
 package fr.diginamic.hello.controleurs;
 
 import fr.diginamic.hello.models.Ville;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/villes")
 public class VilleControleur {
 
+    private List<Ville> villes = new ArrayList<>(List.of(
+            new Ville(1, "Paris", 2148000),
+            new Ville(2, "Lyon", 513000),
+            new Ville(3, "Marseille", 861000),
+            new Ville(4, "Toulouse", 493000),
+            new Ville(5, "Nice", 342000)));
+
     @GetMapping
-    public List<Ville> getVilles(){
-        return Arrays.asList(
-                new Ville("Paris", 2148000),
-                new Ville("Lyon", 513000),
-                new Ville("Marseille", 861000),
-                new Ville("Toulouse", 493000),
-                new Ville("Nice", 342000)
-        );
+    public List<Ville> getVilles() {
+        return villes;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getVilleById(@PathVariable int id) {
+        Optional<Ville> villeATrouver = villes.stream()
+                .filter(v -> v.getId() == id)
+                .findAny();
+        if(villeATrouver.isPresent()) {
+            return ResponseEntity.ok(villeATrouver);
+        } else {
+            return ResponseEntity.status(404).body("ville introuvable");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<String> insertVille(@RequestBody Ville ville) {
+        // Vérifie si l'id existe déjà
+        boolean idExiste = villes.stream()
+                .anyMatch(v -> v.getId() == ville.getId());
+        if(idExiste) {
+            return ResponseEntity.status(404).body("Cet id existe déjà");
+        }
+        // Vérifie si le nom existe déjà
+        boolean nomExiste = villes.stream()
+                        .anyMatch(v -> v.getNom().equalsIgnoreCase(ville.getNom()));
+        if(nomExiste) {
+            return ResponseEntity.status(404).body("Une ville de même nom existe déjà");
+        }
+        // Si aucun doublon, ajoute la ville
+        villes.add(ville);
+        return ResponseEntity.ok("Ville ajoutée avec succès");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateVille(@PathVariable int id, @RequestBody Ville ville) {
+       for(Ville v : villes) {
+           if(v.getId() == id) {
+               v.setNom(ville.getNom());
+               v.setNbHabitants(ville.getNbHabitants());
+               return ResponseEntity.ok("Ville modifiée avec succès");
+           }
+       }
+       return ResponseEntity.status(404).body("Ville introuvable");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteVille(@PathVariable int id) {
+        for(Ville v : villes) {
+            if(v.getId() == id) {
+                villes.remove(v);
+                return ResponseEntity.ok("Ville supprimée");
+            }
+        }
+        return ResponseEntity.status(404).body("Ville introuvable");
     }
 }
--- src/main/java/fr/diginamic/hello/services/HelloService.java
+++ src/main/java/fr/diginamic/hello/services/HelloService.java
@@ -1,4 +1,4 @@
-package fr.diginamic.hello.service;
+package fr.diginamic.hello.services;
 
 import org.springframework.stereotype.Service;
 
